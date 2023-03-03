b5b351f6c6b75cd9d3e318701d793a8ee7955e2c
AndreImasato <andresimasato@gmail.com>
2023-03-03 16:55:22 -0300

    Sets up Login page base structure

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,8 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+// MUI imports
+import { 
+  Avatar,
+  Button,
+  CssBaseline,
+  TextField,
+  Box, 
+  Typography,
+  Container,
+} from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme();
 
 const Login = (props) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleSubmit = () => {
+    console.log("Enviando");
+  }
+
   return (
-    <div>Login</div>
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Entrar em TaskTime
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Endereço de E-Mail"
+              name="email"
+              autoFocus
+              autoComplete="email"
+              type="email"
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="password"
+              label="Senha"
+              name="password"
+              autoComplete="current-password"
+              type={showPassword ? "text" : "password"}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Entrar
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
   )
 }
 
