5c95eae97dd9befd441b52f1dce494025e999ff7
AndreImasato <andresimasato@gmail.com>
2023-03-05 20:51:07 -0300

    Sets up add task button

--- src/components/tasktime/projects/ProjectAddButton.js
+++ src/components/tasktime/projects/ProjectAddButton.js
@@ -4,7 +4,6 @@ import { motion } from 'framer-motion';
 
 // MUI imports
 import { 
-  Grid,
   IconButton,
   Tooltip
 } from '@mui/material';
--- src/components/tasktime/tasks/TaskAddButton.js
+++ src/components/tasktime/tasks/TaskAddButton.js
@@ -1,8 +1,47 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { motion } from 'framer-motion';
+
+// MUI imports
+import { 
+  IconButton,
+  Tooltip
+} from '@mui/material';
+import AddIcon from '@mui/icons-material/Add'
+
+// Reducers imports
+import { setIsModalOpen } from 'src/store/slices/projects/tasksSlice';
 
 const TaskAddButton = (props) => {
+  const dispatch = useDispatch();
+
   return (
-    <div>TaskAddButton</div>
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+    >
+      <Tooltip title="Adicionar nova tarefa">
+        <IconButton
+          onClick={() => {
+            dispatch(setIsModalOpen(true));
+          }}
+          sx={{
+            backgroundColor: (theme) => theme.palette.primary.main,
+            '&:hover': {
+              backgroundColor: (theme) => theme.palette.primary.light
+            },
+            boxShadow: 3
+          }}
+        >
+          <AddIcon
+            sx={{
+              fontSize: 32,
+              color: (theme) => theme.palette.primary.contrastText
+            }}
+          />
+        </IconButton>
+      </Tooltip>
+    </motion.div>
   )
 }
 
