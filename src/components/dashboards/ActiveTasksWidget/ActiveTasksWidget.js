f75d75f955c75beb0390f27a09236ff86875d70a
AndreImasato <andresimasato@gmail.com>
2023-03-10 19:51:02 -0300

    Sets up base open tasks carousel

--- src/components/dashboards/ActiveTasksWidget/ActiveTasksWidget.js
+++ src/components/dashboards/ActiveTasksWidget/ActiveTasksWidget.js
@@ -1,8 +1,74 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+import Carousel from 'react-material-ui-carousel'
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+// MUI components
+import { 
+  Box,
+  Paper,
+  Card,
+  CardContent,
+  Typography,
+  CardActions
+} from '@mui/material';
+
+// Custom imports
+import Utils from 'src/utils';
 
 const ActiveTasksWidget = (props) => {
+
+  const { openTasks } = useSelector(({ dashboards }) => dashboards.openTasks);
+
   return (
-    <div>ActiveTasksWidget</div>
+    <motion.div
+      style={{ width: "100%" }}
+      initial={{ opacity: 0, scale: 0.6 }}
+      animate={{ opacity: 1, scale: 1 }}s
+    >
+      <Paper
+        sx={{ width: "100%", padding: 2 }}
+      >
+        <Box>
+          <Typography variant="h6">Tarefas Abertas</Typography>
+          {openTasks.length > 0
+            ? (
+              <Carousel>
+                {openTasks.map((ot) => (
+                  <Card
+                    key={ot.public_id}
+                  >
+                    <CardContent>
+                      <Typography sx={{ fontSize: 14 }}>{ot.name}</Typography>
+                      <Typography sx={{ fontSize: 16 }}>{Utils.parseTimeIntervalToString(ot.duration)}</Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Link
+                        to={`/projects/${ot.project_public_id}/${ot.public_id}`}
+                        style={{
+                          textDecoration: 'none'
+                        }}
+                      >
+                        <Typography
+                          sx={{
+                            color: (theme) => theme.palette.primary.main
+                          }}
+                        >
+                          DETALHES
+                        </Typography>
+                      </Link>
+                    </CardActions>
+                  </Card>
+                ))}
+              </Carousel>
+            )
+            : (<Typography variant="body1">Nenhuma Tarefa em Aberto!</Typography>)
+          }
+
+        </Box>
+      </Paper>
+    </motion.div>
   )
 }
 
--- src/components/dashboards/RankingWidget/RankingWidget.js
+++ src/components/dashboards/RankingWidget/RankingWidget.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
-import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux';
 
 // MUI components
 import { 
