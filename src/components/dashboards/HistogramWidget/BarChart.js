4d480dea1d5ef1722fb7cdba114d40d855ffcd0f
AndreImasato <andresimasato@gmail.com>
2023-03-13 16:47:52 -0300

    Fixes charts styling

--- src/components/dashboards/HistogramWidget/BarChart.js
+++ src/components/dashboards/HistogramWidget/BarChart.js
@@ -2,21 +2,30 @@ import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import _ from 'lodash';
 import ReactApexChart from 'react-apexcharts';
+import moment from 'moment';
+
+// Custom imports
+import Utils from 'src/utils';
 
 const BarChart = (props) => {
   const { timeData, period } = useSelector(({ dashboards }) => dashboards.histogram);
   const [series, setSeries] = useState({});
   const [options, setOptions] = useState({});
 
+  const formatXLabels = (value) => {
+    switch (period){
+      case 'year':
+        return Utils.capitalizeFirstLetter(moment(value, 'MM').format('MMM'));
+      default:
+        // week and month
+        return moment(value, 'YYYY-MM-DD').format('DD/MM/YYYY');
+    }
+  }
+
   useEffect(() => {
     if (!_.isEmpty(timeData[period])){
       const { plot_data } = timeData[period];
 
-      //TODO deals with category depending on the selected period
-      //TODO if week, then name of the days of the week
-      //TODO if month, then format days to pt-br format
-      //TODO if year, format to months names
-      //TODO formats interval duration
       const newOptions = {
         chart: {
           height: "100%",
@@ -24,12 +33,40 @@ const BarChart = (props) => {
         },
         plotOptions: {
           bar: {
-            columnWidth: "90%",
-            horizontal: false
+            columnWidth: "25%",
+            horizontal: false,
+            borderRadius: 5,
+            dataLabels: {
+              position: 'top',
+            },
           }
         },
         xaxis: {
-          categories: plot_data.xaxis
+          categories: plot_data.xaxis,
+          axisBorder: {
+            show: false,
+          },
+          labels: {
+            show: true,
+            formatter: formatXLabels
+          },
+          tooltip: {
+            enabled: false,
+          },
+        },
+        yaxis: {
+          axisBorder: {
+            show: false,
+          },
+          axisTicks: {
+            show: false,
+          },
+          labels: {
+            show: false,
+            formatter: (val) => {
+              return Utils.parseTimeIntervalToString(val);
+            }
+          },
         },
         fill: {
           opacity: 1
@@ -38,12 +75,21 @@ const BarChart = (props) => {
           show: false
         },
         dataLabels: {
-          enabled: false
+          enabled: true,
+          formatter: (val) => {
+            return Utils.parseTimeIntervalToString(val);
+          },
+          offsetY: -20,
+          style: {
+            fontSize: 12,
+            colors: ['#304758']
+          }
         }
       };
       setOptions(newOptions);
 
       const newSeries = [{
+        name: "Tempo de Atividade",
         data: plot_data.series
       }]
       setSeries(newSeries);
