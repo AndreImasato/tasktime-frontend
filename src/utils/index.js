ff326c9cc7b002dc5159a121dd27f5b234156a28
AndreImasato <andresimasato@gmail.com>
2023-03-20 21:31:08 -0300

    Applies parse total duration function

--- src/utils/index.js
+++ src/utils/index.js
@@ -18,7 +18,7 @@ class Utils {
     const hoursRemainder = interval % 3600;
     const minutes = ~~(hoursRemainder / 60);
     const seconds = hoursRemainder % 60;
-    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    return `${hours.toFixed(0).padStart(2, '0')}:${minutes.toFixed(0).padStart(2, '0')}:${seconds.toFixed(0).padStart(2, '0')}`;
   }
 }
 
